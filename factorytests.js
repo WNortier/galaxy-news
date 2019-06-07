38d764a8ea3964b9f48efc50fa26a97c383d339d
root <root@MSI.localdomain>
2019-06-07 11:48:01 +0200

    js working2

--- factorytests.js
+++ factorytests.js
@@ -7,3 +7,13 @@ describe('greet function' , function(){
     });
 
 })
+
+describe('greet function' , function(){
+    
+    it("Should return 'Hello John'.", function(){
+        var factoryVariable = Greeting();
+        factoryVariable.language("English");
+        assert.deepEqual("Hello, John", factoryVariable.greet("John"));
+    });
+
+})
